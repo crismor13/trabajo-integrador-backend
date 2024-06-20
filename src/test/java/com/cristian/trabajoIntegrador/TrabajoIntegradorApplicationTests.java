624e24583c530709318fb9718ae99c79bdc2daa9
Cristian <crismonsalve4@gmail.com>
2024-06-19 21:54:47 -0500

    Creo proyecto con Spring Initialzr

---
+++ src/test/java/com/cristian/trabajoIntegrador/TrabajoIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristian.trabajoIntegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
